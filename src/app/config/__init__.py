4a4b3184b0dd39d7b1a019d7151e8711e62a0dcd
Breno-MT <brenomtdev@gmail.com>
2025-06-22 02:11:19 -0300

    FEAT: added config classes

--- src/app/config/__init__.py
+++ src/app/config/__init__.py
@@ -1,2 +1,35 @@
 import os
-from 
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+class Development(object):
+    TESTING = False
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MONGO_URI = os.getenv("MONGO_URI")
+
+class Testing(object):
+    TESTING = True
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MONGO_URI = os.getenv("MONGO_URI")
+
+class Production(object):
+    TESTING = False
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MONGO_URI = os.getenv("MONGO_URI")
+
+class Holomogation(object):
+    TESTING = False
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MONGO_URI = os.getenv("MONGO_URI")
+
+app_config = {
+    "development": Development,
+    "testing": Testing,
+    "production": Production,
+    "homologation": Holomogation
+}
