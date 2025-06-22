b194aa2d1aa3cd0cf053951ef34df3f4252f5b06
Breno-MT <brenomtdev@gmail.com>
2025-06-22 02:29:47 -0300

    FEAT: configured app init file

---
+++ src/app/__init__.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+from src.app.config import app_config
+from src.app.utils import mongo
+
+app = Flask(__name__)
+app.config.from_object(app_config[os.getenv("FLASK_ENV")])
+mongo.init_app(app)
+mongo_client_users = mongo.cx.get_database("users")
+
+CORS(app)
