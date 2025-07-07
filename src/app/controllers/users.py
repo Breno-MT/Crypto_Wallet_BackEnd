a4142040f11f14f8be908222aefb4fb7f2172e0f
Breno-MT <brenomtdev@gmail.com>
2025-07-06 23:58:45 -0300

    REFACT: moved create_user logic to a function to make it look better to read it and do maintenance

--- src/app/controllers/users.py
+++ src/app/controllers/users.py
@@ -2,13 +2,14 @@ from bson import json_util
 from flask import Blueprint, request
 from flask.wrappers import Response
 from src.app import mongo_client
+from src.app.utils.users import create_user_db
 
 user = Blueprint("user", __name__, url_prefix="/user")
 
 @user.route("/", methods=["GET"])
 def get_users():
     try:
-        all_users = mongo_client.users.find()
+        all_users = mongo_client.users.find({}, {"password": False})
 
         return Response(
             response=json_util.dumps({"records": all_users}),
@@ -26,23 +27,17 @@ def get_users():
 @user.route("/create_user", methods=["POST"])
 def create_user():
     """
-    This URL expects "email, password" in order to create the user.
+    This URL expects "username, email, password" in order to create the user.
     """
     try:
-        new_user = request.get_json()
+        response = create_user_db(request.get_json())
 
-        mongo_client.users.insert_one(new_user)
+        return response
 
-        return Response(
-            response=json_util.dumps({"records": new_user}),
-            status=201,
-            mimetype="application/json"
-        )
-
-    except Exception as _:
+    except Exception as e:
         
         return Response(
-            response=json_util.dumps({"error": f"Something happened! Check your json body!"}),
+            response=json_util.dumps({"error": f"Something happened! Check your json body! {e}"}),
             status=403,
             mimetype="application/json"
         )
