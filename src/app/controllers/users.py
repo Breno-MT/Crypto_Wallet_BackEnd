6c86ce93323b9f719f5f72f2ce84bc20f2586ef0
Breno-MT <brenomtdev@gmail.com>
2025-07-07 00:11:25 -0300

    FEAT: added new endpoint(validate_token, login)

--- src/app/controllers/users.py
+++ src/app/controllers/users.py
@@ -2,12 +2,12 @@ from bson import json_util
 from flask import Blueprint, request
 from flask.wrappers import Response
 from src.app import mongo_client
-from src.app.utils.users import create_user_db
+from src.app.utils.users import create_user_db, login_user, validate_token_user
 
 user = Blueprint("user", __name__, url_prefix="/user")
 
 @user.route("/", methods=["GET"])
-def get_users():
+def get_users_endpoint():
     try:
         all_users = mongo_client.users.find({}, {"password": False})
 
@@ -25,20 +25,43 @@ def get_users():
         )
 
 @user.route("/create_user", methods=["POST"])
-def create_user():
+def create_user_endpoint():
     # TODO: transform create_user into register_user
     """
     This URL expects "username, email, password" in order to create the user.
     """
     try:
         response = create_user_db(request.get_json())
-
         return response
 
     except Exception as e:
-        
         return Response(
             response=json_util.dumps({"error": f"Something happened! Check your json body! {e}"}),
             status=403,
             mimetype="application/json"
         )
+
+@user.route("/login", methods=["POST"])
+def login_user_endpoint():
+    """
+    The user has to login using username/email and password
+    """
+    try:
+        response = login_user(request.get_json())
+        return response
+
+    except Exception as e:
+        return Response(
+            response=json_util.dumps({"error": f"Oops! Something happened! {e}"}),
+            status=403,
+            mimetype="application/json"
+        )
+
+@user.route("/validate_token", methods=["GET"])
+def validate_token_endpoint():
+    validate_token_user()
+    return Response(
+        response=json_util.dumps({"message": "Token is valid (W.I.P)"}),
+        status=200,
+        mimetype="application/json"
+    )
