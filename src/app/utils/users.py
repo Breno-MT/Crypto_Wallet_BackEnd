dc976554cb1265acc4089176136702d50f997e97
Breno-MT <brenomtdev@gmail.com>
2025-07-06 23:57:34 -0300

    FEAT: created function for user creation in the database

---
+++ src/app/utils/users.py
@@ -0,0 +1,38 @@
+import bcrypt
+from bson import json_util
+from flask.wrappers import Response
+import os
+from src.app import mongo_client
+from src.app.config import app_config
+
+def create_user_db(user_body):
+    try:
+        is_email_already_created = mongo_client.users.find_one({"email": user_body["email"]})
+        is_username_already_created = mongo_client.users.find_one({"username": user_body["username"]})
+
+        if is_email_already_created or is_username_already_created:
+            return Response(
+                response=json_util.dumps({"error": "User already exists in the database."}),
+                status=409,
+                mimetype="application/json"
+            )
+
+        user_body["password"] = bcrypt.hashpw(
+            password=user_body["password"].encode("utf-8"),
+            salt=bcrypt.gensalt(10)
+        )
+
+        mongo_client.users.insert_one(user_body)
+
+        return Response(
+            response=json_util.dumps({"records": user_body}),
+            status=200,
+            mimetype="application/json"
+        )
+
+    except Exception as e:
+        return Response(
+            response=json_util.dumps({"error": f"{e}"}),
+            status=400,
+            mimetype="application/json"
+        )
