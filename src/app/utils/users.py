91273f824379a718710e1e45b708e1308a6dc9fe
Breno-MT <brenomtdev@gmail.com>
2025-08-04 19:18:17 -0300

    FEAT: added function to encrypt user pwd, created login validation, tweaked register user logic

--- src/app/utils/users.py
+++ src/app/utils/users.py
@@ -3,10 +3,11 @@ from bson import json_util
 from flask.wrappers import Response
 import os
 from src.app import mongo_client
-from src.app.config import app_config
+from src.app.utils.token_jwt import create_token 
 
 def register_user_db(user_body):
     try:
+        # TODO: improve this line of code to one single function
         is_email_already_created = mongo_client.users.find_one({"email": user_body["email"]})
         is_username_already_created = mongo_client.users.find_one({"username": user_body["username"]})
 
@@ -17,10 +18,7 @@ def register_user_db(user_body):
                 mimetype="application/json"
             )
 
-        user_body["password"] = bcrypt.hashpw(
-            password=user_body["password"].encode("utf-8"),
-            salt=bcrypt.gensalt(10)
-        )
+        user_body["password"] = encrypt_user_password(user_body["password"])
 
         mongo_client.users.insert_one(user_body)
         user_body.pop("password")
@@ -38,8 +36,39 @@ def register_user_db(user_body):
             mimetype="application/json"
         )
 
-def check_password_user(user_body): pass
+def login_user(user_body): 
+    is_user_valid = validate_user_login(user_body)
 
-def login_user(user_body): pass
+    if not is_user_valid:
+        return Response(
+            response=json_util.dumps({"error": "Invalid credentials."}),
+            status=401,
+            mimetype="application/json"
+        )
+
+    del user_body["password"]
+
+    return Response(
+        response=json_util.dumps({"token": create_token(user_body)}),
+        status=200,
+        mimetype="application/json"
+    )
+
+
+def validate_user_login(user_body):
+    get_user = mongo_client.users.find_one({"email": user_body["email"]})
+
+    if not get_user:
+        return False
+
+    is_user_valid = bcrypt.checkpw(user_body["password"].encode("utf-8"), get_user.get("password"))
+
+    return is_user_valid
 
 def validate_token_user(token): pass
+
+def encrypt_user_password(password):
+    return bcrypt.hashpw(
+                password=password,
+                salt=bcrypt.gensalt(10)
+            )
